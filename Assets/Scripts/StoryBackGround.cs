790ecc53b03bc90ff1f0bd474fabdfcdbbb89edb
aokitw513 <asdwkuiop@gmail.com>
2025-04-26 16:37:06 +0800

    Just A BackGround Alpha
    U can use this one to enter story or exit

--- Assets/Scripts/StoryBackGround.cs
+++ Assets/Scripts/StoryBackGround.cs
@@ -3,21 +3,58 @@ using UnityEngine;
 
 public class StoryBackGround : MonoBehaviour
 {
-    [SerializeField] Image image;
-    private float speed = 0.5f;
+    [SerializeField] GameObject BackGroundObject;
+
+    private Color BackGroundColor;
+    [SerializeField] float speed = 4f;
+
+    [SerializeField] bool IsEntering = false;
+    [SerializeField] bool IsExiting = false;
+
+    private void Start()
+    {
+        BackGroundColor = BackGroundObject.GetComponent<Image>().color;
+        BackGroundColor.a = 1f;
+        BackGroundObject.GetComponent<Image>().color = BackGroundColor;
+    }
+
 
     private void Update()
     {
-        
+        if(IsEntering)
+        {
+            if(BackGroundColor.a <= 1)
+            {
+                BackGroundColor.a += speed * Time.deltaTime;
+                BackGroundObject.GetComponent<Image>().color = BackGroundColor;   
+                Debug.Log(BackGroundColor.a);
+            }
+            else
+            {
+                IsEntering = false;
+            }
+        }
+        else if(IsExiting)
+        {
+            if(BackGroundColor.a >= 0)
+            {
+                BackGroundColor.a -= speed * Time.deltaTime;
+                BackGroundObject.GetComponent<Image>().color = BackGroundColor;   
+            }
+            else
+            {
+                IsExiting = false;
+            }
+        }
     }
 
     public void EnterStory()
     {
-
+        IsEntering = true;
     }
 
     public void ExitStory()
     {
-
+        IsExiting = true;
     }
 }
